0b23693c4c0f77d06b1d0499035c1cb30baad2d4
Owen Robison <orobison@proton.me>
2025-02-12 08:52:12 -0500

    add display functionality

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const tbody = document.querySelector("tbody");
 const addBook = document.querySelector("#addBook");
 const dialog = document.querySelector("dialog");
 const dialogClose = document.querySelector("#closeDialog");
+const submitNewBook = document.querySelector("#submitBook");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#numPages");
+const readInput = document.querySelector("#isRead");
 
 addBook.addEventListener("click", () => {
     dialog.showModal();
@@ -12,11 +17,21 @@ dialogClose.addEventListener("click", () => {
     dialog.close();
 });
 
+submitNewBook.addEventListener("click", (e) => {
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
+    titleInput.value = authorInput.value = pagesInput.value = "";
+    readInput.checked = false;
+    e.preventDefault();
+    dialog.close();
+    displayLibrary();
+});
+
 function Book(title, author, numPages, isRead) {
     this.title = title;
     this.author = author;
     this.numPages = numPages;
     this.isRead = isRead;
+    this.isDisplayed = false;
 }
 
 function addBookToLibrary(title, author, numPages, isRead) {
@@ -26,11 +41,14 @@ function addBookToLibrary(title, author, numPages, isRead) {
 
 function displayLibrary() {
     for (const book of library) {
-        const newRow = tbody.insertRow();
-        for (const prop in book) {
-            const newCell = newRow.insertCell();
-            const newText = document.createTextNode(book[prop]);
-            newCell.appendChild(newText);
+        if (!book.isDisplayed) {
+            const newRow = tbody.insertRow();
+            for (const prop in book) {
+                const newCell = newRow.insertCell();
+                const newText = document.createTextNode(book[prop]);
+                newCell.appendChild(newText);
+            }
+            book.isDisplayed = true;
         }
     }
 }
